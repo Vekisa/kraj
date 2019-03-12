23f21c4ea2b441a9fd7ed7d88528d3b3c5dfc394
Vekisa <vekisa123>
2019-03-12 14:45:38 +0100

    Initial commit

---
+++ src/main/java/xmlb/XmlbApplication.java
@@ -0,0 +1,13 @@
+package xmlb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xmlb/XmlbApplicationTests.java
@@ -0,0 +1,16 @@
+package xmlb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XmlbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
