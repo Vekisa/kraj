2e21ba77e7cf81f504e752ced8ea2c1c2f970be8
Vekisa <vekisa123>
2019-03-18 20:36:10 +0100

    pocetna stranica, sign in, sign up

--- src/main/front/src/app/app-routing.module.ts
+++ src/main/front/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {RegistrationComponent} from './registration/registration.component';
+import {WelcomeComponent} from './welcome/welcome.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: WelcomeComponent
+  },
+  {
+    path: 'sign_in',
     component: LoginComponent
+  },
+  {
+    path: 'sign_up',
+    component: RegistrationComponent
   }
 ];
 @NgModule({
--- src/main/front/src/app/app.module.ts
+++ src/main/front/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import {AppRoutingModule} from './app-routing.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { RegistrationComponent } from './registration/registration.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegistrationComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
