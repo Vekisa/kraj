c81d5d2d024b60afa46f3508336fa2ed867a9a8f
Levarsky <Mac>
2019-03-18 23:25:51 +0100

    Move package

--- src/main/java/xmlb/security/SecurityConfig.java
+++ src/main/java/xmlb/security/SecurityConfig.java
@@ -1,12 +1,10 @@
-package security;
+package xmlb.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
