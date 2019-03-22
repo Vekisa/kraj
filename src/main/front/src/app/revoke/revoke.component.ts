6988ff825c106fca080c03a0948c967359f541b6
TomicI <ivanatomic3@uns.ac.rs>
2019-03-22 17:46:25 +0100

    Revoke front

---
+++ src/main/front/src/app/revoke/revoke.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {CertificateService} from "../service/certificate.service";
+
+import {Observable} from "rxjs";
+import {CertInfo} from "../model";
+
+@Component({
+  selector: 'app-revoke',
+  templateUrl: './revoke.component.html',
+  styleUrls: ['./revoke.component.css']
+})
+export class RevokeComponent implements OnInit {
+
+  constructor( private certificateService: CertificateService) { }
+
+  certificates: Observable<CertInfo[]>;
+
+  ngOnInit() {
+    this.certificates = this.certificateService.allCertificates();
+  }
+
+  revoke(alias: string){
+    this.certificateService.revokeCertificate(alias);
+  }
+}
