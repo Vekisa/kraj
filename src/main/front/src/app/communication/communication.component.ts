d773f01958aa9b3dbb63ac7bc600bedc66256dfa
TomicI <ivanatomic3@uns.ac.rs>
2019-03-23 00:22:27 +0100

    Communication front

--- src/main/front/src/app/communication/communication.component.ts
+++ src/main/front/src/app/communication/communication.component.ts
@@ -12,10 +12,13 @@ export class CommunicationComponent implements OnInit {
   comForm: FormGroup;
   cert: CertInfo[];
 
+  fS:number;
+  first: string;
+  second: string;
   constructor(private formBuilder: FormBuilder, private certificateService: CertificateService) { }
 
   ngOnInit() {
-
+    this.fS=1;
     this.comForm = this.formBuilder.group({
       first: [''],
       second:['']
@@ -28,6 +31,20 @@ export class CommunicationComponent implements OnInit {
 
   rowSelected(cert:any){
     //this.comForm.value.parent = cert;
+    console.log("selektovan red " + cert);
+    if(this.fS==1){
+      this.comForm.value.first = cert;
+      this.first=cert;
+    }else{
+      this.comForm.value.second = cert;
+      this.second=cert;
+    }
+
+  }
+
+  buttonR(broj: number){
+    console.log("selektovano " + broj);
+    this.fS=broj;
   }
 
 }
