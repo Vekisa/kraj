8f128d71e573730891354762770a424712093780
Vekisa <vekisa123>
2019-06-01 18:36:03 +0200

    ubacene komponente

--- Front/frontadmin/src/app/app-routing.module.ts
+++ Front/frontadmin/src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {NavigationComponent} from "./navigation/navigation.component";
 import {ObjectTypesComponent} from "./object-types/object-types.component";
+import {AccommodationTypesComponent} from "./accommodation-types/accommodation-types.component";
+import {ExtraOptionsComponent} from "./extra-options/extra-options.component";
+import {CommentsComponent} from "./comments/comments.component";
+import {UsersComponent} from "./users/users.component";
+import {AgentsComponent} from "./agents/agents.component";
 
 const routes: Routes = [
   {
@@ -11,6 +16,26 @@ const routes: Routes = [
   {
     path: 'object_types',
     component: ObjectTypesComponent
+  },
+  {
+    path: 'accommodation_types',
+    component: AccommodationTypesComponent
+  },
+  {
+    path: 'extra_options',
+    component: ExtraOptionsComponent
+  },
+  {
+    path: 'comments',
+    component: CommentsComponent
+  },
+  {
+    path: 'users',
+    component: UsersComponent
+  },
+  {
+    path: 'agents',
+    component: AgentsComponent
   }
 ];
 
