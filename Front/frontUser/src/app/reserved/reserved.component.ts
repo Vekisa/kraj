25fd55e0fe9ed48573d63f8c523b5ec66204bbad
Admin <stefanapetrovic18@uns.ac.rs>
2019-06-28 13:44:50 +0200

    frontUser: Category, Rating

--- Front/frontUser/src/app/reserved/reserved.component.ts
+++ Front/frontUser/src/app/reserved/reserved.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-reserved',
   templateUrl: './reserved.component.html',
-  styleUrls: ['./reserved.component.css']
+  styleUrls: ['./reserved.component.css'],
+  providers: [NgbRatingConfig]
 })
 export class ReservedComponent implements OnInit {
   currentRate = 1;
 
-  constructor() { }
+  constructor(config: NgbRatingConfig) {}
 
   ngOnInit() {
   }
